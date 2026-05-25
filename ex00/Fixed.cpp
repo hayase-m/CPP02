774e4e5e9ee3d11835bbd51b5419908dd8cb2773
Hayase Maruyama <mrym.124.hys@gmail.com>
2026-05-25 15:04:03 +0900

    ex00のFixedクラスを実装

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,26 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed(void) : _rawBits(0) {
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) : _rawBits(other._rawBits) {
+  std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &other) {
+  std::cout << "Copy assignment operator called" << std::endl;
+  if (this != &other)
+    this->_rawBits = other._rawBits;
+  return *this;
+}
+
+Fixed::~Fixed(void) { std::cout << "Destructor called" << std::endl; }
+
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return _rawBits;
+}
+
+void Fixed::setRawBits(int const raw) { _rawBits = raw; }

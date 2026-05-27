b32f260c81a411dee2e57254bf9b68a7d5b14529
Hayase Maruyama <mrym.124.hys@gmail.com>
2026-05-27 19:26:04 +0900

    headerづくり

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,51 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+private:
+  int _rawBits;
+  static const int _fractionalBits = 8;
+
+public:
+  Fixed(void);
+  Fixed(const Fixed &other);
+  Fixed &operator=(const Fixed &other);
+  ~Fixed(void);
+
+  Fixed(const int value);
+  Fixed(const float value);
+
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+
+  int toInt(void) const;
+  float toFloat(void) const;
+
+  bool operator>(const Fixed &rhs) const;
+  bool operator<(const Fixed &rhs) const;
+  bool operator>=(const Fixed &rhs) const;
+  bool operator<=(const Fixed &rhs) const;
+  bool operator==(const Fixed &rhs) const;
+  bool operator!=(const Fixed &rhs) const;
+
+  Fixed operator+(const Fixed &rhs) const;
+  Fixed operator-(const Fixed &rhs) const;
+  Fixed operator*(const Fixed &rhs) const;
+  Fixed operator/(const Fixed &rhs) const;
+
+  Fixed &operator++(void);
+  Fixed operator++(int);
+  Fixed &operator--(void);
+  Fixed operator--(int);
+
+  static Fixed &min(Fixed &lhs, Fixed &rhs);
+  static const Fixed &min(const Fixed &lhs, const Fixed &rhs);
+  static Fixed &max(Fixed &lhs, Fixed &rhs);
+  static const Fixed &max(const Fixed &lhs, const Fixed &rhs);
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+
+#endif
